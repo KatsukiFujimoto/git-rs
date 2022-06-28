599c2a77e177fcd5fc24f6f80f22597e59128e34
KatsukiFujimoto <kf1996cnbl@gmail.com>
2022-06-28 09:41:20 +0900

    レンダリングするTableの行に対して取り敢えず矢印キーで選択行を制御できるようにしたのと、qでexitできるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,119 @@
 use crossterm::{
-    event::{DisableMouseCapture, EnableMouseCapture},
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::{io, thread, time::Duration};
+use std::io;
 use tui::{
-    backend::CrosstermBackend,
-    layout::Constraint,
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Layout},
     style::{Color, Modifier, Style},
-    widgets::{Block, Row, Table},
-    Terminal,
+    widgets::{Block, Borders, Cell, Row, Table, TableState},
+    Frame, Terminal,
 };
+
+struct App<'a> {
+    state: TableState,
+    items: Vec<Vec<&'a str>>,
+}
+impl<'a> App<'a> {
+    fn new() -> App<'a> {
+        App {
+            state: TableState::default(),
+            items: vec![
+                vec!["Row1_1", "Row1_2", "Row1_3"],
+                vec!["Row2_1", "Row2_2", "Row2_3"],
+                vec!["Row3_1", "Row3_2", "Row3_3"],
+                vec!["Row4_1", "Row4_2", "Row4_3"],
+                vec!["Row5_1", "Row5_2", "Row5_3"],
+                vec!["Row6_1", "Row6_2", "Row6_3"],
+                vec!["Row7_1", "Row7_2", "Row7_3"],
+                vec!["Row8_1", "Row8_2", "Row8_3"],
+                vec!["Row9_1", "Row9_2", "Row9_3"],
+            ],
+        }
+    }
+
+    pub fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    pub fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+}
+
+fn render_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
+    let layout = Layout::default()
+        .constraints([Constraint::Percentage(100)].as_ref())
+        .margin(5)
+        .split(f.size());
+    let normal_style = Style::default().bg(Color::Blue);
+    let selected_style = Style::default().add_modifier(Modifier::REVERSED);
+    let header_cells = ["Header1", "Header2", "Header3"]
+        .iter()
+        .map(|h| Cell::from(*h).style(Style::default().fg(Color::Red)));
+    let header = Row::new(header_cells)
+        .style(normal_style)
+        .height(1)
+        .bottom_margin(1);
+    let rows = app.items.iter().map(|item| {
+        let height = item
+            .iter()
+            .map(|content| content.chars().filter(|c| *c == '\n').count())
+            .max()
+            .unwrap_or(0)
+            + 1;
+        let cells = item.iter().map(|c| Cell::from(*c));
+        Row::new(cells).height(height as u16).bottom_margin(1)
+    });
+    let table = Table::new(rows)
+        .header(header)
+        .block(Block::default().borders(Borders::ALL).title("Table"))
+        .highlight_style(selected_style)
+        .highlight_symbol(">> ")
+        .widths(&[
+            Constraint::Percentage(50),
+            Constraint::Length(30),
+            Constraint::Min(10),
+        ]);
+    f.render_stateful_widget(table, layout[0], &mut app.state)
+}
+
+fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
+    loop {
+        terminal.draw(|f| render_ui(f, &mut app))?;
+
+        if let Event::Key(key) = event::read()? {
+            match key.code {
+                KeyCode::Char('q') => return Ok(()),
+                KeyCode::Down => app.next(),
+                KeyCode::Up => app.previous(),
+                _ => {}
+            }
+        }
+    }
+}
 
 fn main() -> Result<(), io::Error> {
     // setup terminal
@@ -20,38 +123,9 @@ fn main() -> Result<(), io::Error> {
     let backend = CrosstermBackend::new(stdout);
     let mut terminal = Terminal::new(backend)?;
 
-    terminal.draw(|f| {
-        let size = f.size();
-        let table = Table::new(vec![
-            Row::new(vec!["row1_1", "row1_2", "row1_3"]),
-            Row::new(vec!["row2_1", "row2_2", "row2_3"]),
-            Row::new(vec!["row3_1", "row3_2", "row3_3"]),
-            Row::new(vec!["row4_1", "row4_2", "row4_3"]),
-            Row::new(vec!["row5_1", "row5_2", "row5_3"]),
-            Row::new(vec!["row6_1", "row6_2", "row6_3"]),
-            Row::new(vec!["row7_1", "row7_2", "row7_3"]),
-            Row::new(vec!["row8_1", "row8_2", "row8_3"]),
-            Row::new(vec!["row9_1", "row9_2", "row9_3"]),
-        ])
-        .style(Style::default().fg(Color::White))
-        .header(
-            Row::new(vec!["column1", "column2", "column3"])
-                .style(Style::default().fg(Color::Yellow))
-                .bottom_margin(1),
-        )
-        .block(Block::default().title("Table"))
-        .widths(&[
-            Constraint::Length(10),
-            Constraint::Length(10),
-            Constraint::Length(20),
-        ])
-        .column_spacing(1)
-        .highlight_style(Style::default().add_modifier(Modifier::BOLD))
-        .highlight_symbol(">>");
-        f.render_widget(table, size);
-    })?;
-
-    thread::sleep(Duration::from_millis(10000));
+    // create app and run it
+    let app = App::new();
+    let res = run_app(&mut terminal, app);
 
     // restore terminal
     disable_raw_mode()?;
@@ -61,6 +135,10 @@ fn main() -> Result<(), io::Error> {
         DisableMouseCapture
     )?;
     terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err);
+    }
 
     Ok(())
 }
