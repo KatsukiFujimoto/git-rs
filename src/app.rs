49913eea9bef74b336cb577d9faa94d04f3fbdbe
KatsukiFujimoto <kf1996cnbl@gmail.com>
2022-06-30 10:39:36 +0900

    main.rsにはロジックを置きたくないので、アプリケーションのロジックをapp.rsに移動した

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,14 @@
-use tui::widgets::TableState;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::io;
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Block, Borders, TableState},
+    Terminal,
+};
 
 pub struct App<'a> {
     pub state: TableState,
@@ -20,6 +30,45 @@ impl<'a> App<'a> {
                 vec!["Row9_1", "Row9_2", "Row9_3"],
             ],
         }
+    }
+
+    pub fn start() -> Result<(), io::Error> {
+        // setup terminal
+        enable_raw_mode()?;
+        let mut stdout = io::stdout();
+        execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+        let backend = CrosstermBackend::new(stdout);
+        let mut terminal = Terminal::new(backend)?;
+
+        // create app and run it
+        let mut app = App::new();
+        loop {
+            terminal.draw(|f| {
+                let size = f.size();
+                let block = Block::default().title("Block").borders(Borders::ALL);
+                f.render_widget(block, size);
+            })?;
+
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => break,
+                    KeyCode::Down => app.next(),
+                    KeyCode::Up => app.previous(),
+                    _ => {}
+                }
+            }
+        }
+
+        // restore terminal
+        disable_raw_mode()?;
+        execute!(
+            terminal.backend_mut(),
+            LeaveAlternateScreen,
+            DisableMouseCapture
+        )?;
+        terminal.show_cursor()?;
+
+        Ok(())
     }
 
     pub fn next(&mut self) {
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,14 @@
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
 use git_rs::app::App;
 use std::io;
 use tui::{
-    backend::{Backend, CrosstermBackend},
+    backend::Backend,
     layout::{Constraint, Layout},
     style::{Color, Modifier, Style},
     widgets::{Block, Borders, Cell, Row, Table},
-    Frame, Terminal,
+    Frame,
 };
 
+#[allow(dead_code)]
 fn render_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     let layout = Layout::default()
         .constraints([Constraint::Percentage(100)].as_ref())
@@ -50,45 +46,10 @@ fn render_ui<B: Backend>(f: &mut Frame<B>, app: &mut App) {
     f.render_stateful_widget(table, layout[0], &mut app.state)
 }
 
-fn run_app<B: Backend>(terminal: &mut Terminal<B>, mut app: App) -> io::Result<()> {
-    loop {
-        terminal.draw(|f| render_ui(f, &mut app))?;
-
-        if let Event::Key(key) = event::read()? {
-            match key.code {
-                KeyCode::Char('q') => return Ok(()),
-                KeyCode::Down => app.next(),
-                KeyCode::Up => app.previous(),
-                _ => {}
-            }
-        }
-    }
-}
-
 fn main() -> Result<(), io::Error> {
-    // setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    // create app and run it
-    let app = App::new();
-    let res = run_app(&mut terminal, app);
-
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-
+    let res = App::start();
     if let Err(err) = res {
         println!("{:?}", err);
     }
-
     Ok(())
 }
