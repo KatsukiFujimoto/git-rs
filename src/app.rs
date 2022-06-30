6babf757aa05a9f0c0a869188a2a5dc3bd361e49
KatsukiFujimoto <kf1996cnbl@gmail.com>
2022-06-30 23:19:57 +0900

    適当な値をテーブルとして表示していたが、Gitブランチを選択して削除できるようにするために表示する一覧データをGitブランチ名に変えた

--- src/app.rs
+++ src/app.rs
@@ -4,28 +4,25 @@ use crossterm::{
     execute,
     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
 };
-use std::io;
+use git2::{BranchType, Repository};
+use std::{env, io, path::PathBuf};
 use tui::{backend::CrosstermBackend, widgets::TableState, Terminal};
 
-pub struct App<'a> {
+pub struct App {
     pub state: TableState,
-    pub items: Vec<Vec<&'a str>>,
+    pub branch_names: Vec<String>,
 }
-impl<'a> App<'a> {
-    pub fn new() -> App<'a> {
+impl App {
+    pub fn new(path: PathBuf) -> App {
+        let repository = Repository::open(path).unwrap();
+        let branch_names = repository
+            .branches(Some(BranchType::Local))
+            .unwrap()
+            .map(|x| x.unwrap().0.name().unwrap().unwrap().to_string())
+            .collect();
         App {
             state: TableState::default(),
-            items: vec![
-                vec!["Row1_1", "Row1_2", "Row1_3"],
-                vec!["Row2_1", "Row2_2", "Row2_3"],
-                vec!["Row3_1", "Row3_2", "Row3_3"],
-                vec!["Row4_1", "Row4_2", "Row4_3"],
-                vec!["Row5_1", "Row5_2", "Row5_3"],
-                vec!["Row6_1", "Row6_2", "Row6_3"],
-                vec!["Row7_1", "Row7_2", "Row7_3"],
-                vec!["Row8_1", "Row8_2", "Row8_3"],
-                vec!["Row9_1", "Row9_2", "Row9_3"],
-            ],
+            branch_names,
         }
     }
 
@@ -38,10 +35,11 @@ impl<'a> App<'a> {
         let mut terminal = Terminal::new(backend)?;
 
         // create app and run it
-        let mut app = App::new();
+        let path = env::current_dir().unwrap();
+        let mut app = App::new(path);
         loop {
             terminal.draw(|f| {
-                TableSample::render(f, &mut app);
+                TableSample::render(f, &mut app.state, &app.branch_names);
             })?;
 
             if let Event::Key(key) = event::read()? {
@@ -69,7 +67,7 @@ impl<'a> App<'a> {
     pub fn next(&mut self) {
         let i = match self.state.selected() {
             Some(i) => {
-                if i >= self.items.len() - 1 {
+                if i >= self.branch_names.len() - 1 {
                     0
                 } else {
                     i + 1
@@ -84,7 +82,7 @@ impl<'a> App<'a> {
         let i = match self.state.selected() {
             Some(i) => {
                 if i == 0 {
-                    self.items.len() - 1
+                    self.branch_names.len() - 1
                 } else {
                     i - 1
                 }
