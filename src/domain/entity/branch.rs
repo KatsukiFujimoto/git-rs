b1661b2a41d7a76d21bd2725f91760e004ed110d
KatsukiFujimoto <kf1996cnbl@gmail.com>
2022-07-17 00:55:46 +0900

    ブランチのタイプも表示できるようにするために、まずはenum方のブランチタイプを文字列として取得できるようにした

--- src/domain/entity/branch.rs
+++ src/domain/entity/branch.rs
@@ -4,8 +4,38 @@ pub struct Branch {
     pub branch_type: BranchType,
 }
 
+impl Branch {
+    pub fn branch_type(&self) -> String {
+        match self.branch_type {
+            BranchType::Local => "local".into(),
+            BranchType::Remote => "remote".into(),
+        }
+    }
+}
+
 #[derive(Debug, PartialEq, Clone)]
 pub enum BranchType {
     Local,
     Remote,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::domain::entity::fixtures::branch_sample;
+    use pretty_assertions::assert_eq;
+
+    #[test]
+    pub fn test_branch_type() {
+        let branch_local = Branch {
+            branch_type: BranchType::Local,
+            ..branch_sample()
+        };
+        let branch_remote = Branch {
+            branch_type: BranchType::Remote,
+            ..branch_sample()
+        };
+        assert_eq!(branch_local.branch_type(), "local".to_string());
+        assert_eq!(branch_remote.branch_type(), "remote".to_string());
+    }
 }
